18011f13e4a9b1a332a1d53b564e857873d0299f
Ezro <justinmvalentin@gmail.com>
2022-02-27 22:51:09 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='d2r-image',
-    version='0.0.13',
+    version='0.0.14',
     description='A package for parsing items on the ground and hovered tooltips from Diablo II: Resurrected',
     long_description=open('README.md'),
     url="https://github.com/Ezro/d2r_image",
